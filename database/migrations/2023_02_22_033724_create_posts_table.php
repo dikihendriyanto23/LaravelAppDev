f9805671f28d5cc39962218b23641263c9637c3f
Muhammad Diki Hendriyanto <dikihendriyanto23@gmail.com>
2023-02-23 14:29:54 +0700

    second commit 23 feb 2023

--- database/migrations/2023_02_22_033724_create_posts_table.php
+++ database/migrations/2023_02_22_033724_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id');
             $table->string('title');
             $table->text('slug')->unique();
             $table->text('excerpt');
